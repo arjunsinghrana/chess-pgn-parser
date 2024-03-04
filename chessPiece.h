be75c07786733f1ae3c6068b475fb81d2281dae8
Arjun Singh <arjunsingh.rf@gmail.com>
2024-03-04 01:14:19 +0000

    Add class for chess piece

---
+++ chessPiece.h
@@ -0,0 +1,21 @@
+#ifndef CHESSPIECE_H
+#define CHESSPIECE_H
+
+enum ChessPiece
+{
+    EMPTY,
+    WHITE_PAWN,
+    WHITE_ROOK,
+    WHITE_KNIGHT,
+    WHITE_BISHOP,
+    WHITE_QUEEN,
+    WHITE_KING,
+    BLACK_PAWN,
+    BLACK_ROOK,
+    BLACK_KNIGHT,
+    BLACK_BISHOP,
+    BLACK_QUEEN,
+    BLACK_KING
+};
+
+#endif // CHESSPIECE_H
