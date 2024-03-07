bc8e9e96ab92782bffbf73579465952bf3899366
Arjun Singh <arjunsingh.rf@gmail.com>
2024-03-07 23:56:20 +0000

    Add test for en passant

--- test/test_chess_board.cpp
+++ test/test_chess_board.cpp
@@ -55,12 +55,41 @@ TEST(ChessBoardTest, MovePawns) {
     EXPECT_EQ(board.toString(), ss.str());
 }
 
+TEST(ChessBoardTest, EnPassant) {
+    
+    filesystem::path currentDir = filesystem::current_path();
+    filesystem::path filePath = currentDir / "../../test/pgn/captures" / "pawn_en_passant.pgn";
+
+    // Concatenate all chess moves into a single string
+    const string concatenatedChessMoves = Utils::parseFileAndConcatenateChessMoves(filePath.string());
+
+    // Convert string containing moves into vector of Chess Turns
+    const vector<ChessTurn> turns = Utils::parseChessMoves(concatenatedChessMoves);
+
+    ChessBoard board;
+    for (const auto& turn : turns)
+    {
+        board.applyTurn(turn);
+    }
+
+    stringstream ss;
+    ss << "bR|bN|bB|bQ|bK|bB|bN|bR|" << endl;
+    ss << "bP|bP|bP|  |bP|bP|bP|  |" << endl;
+    ss << "  |  |  |wP|  |  |  |  |" << endl;
+    ss << "  |  |  |  |  |  |  |bP|" << endl;
+    ss << "  |  |  |  |  |  |  |  |" << endl;
+    ss << "  |  |  |  |  |  |  |  |" << endl;
+    ss << "wP|wP|wP|wP|  |wP|wP|wP|" << endl;
+    ss << "wR|wN|wB|wQ|wK|wB|wN|wR|" << endl;
+    ss << endl;
+
+    EXPECT_EQ(board.toString(), ss.str());
+}
+
 TEST(ChessBoardTest, FischerSpassky) {
 
     filesystem::path currentDir = filesystem::current_path();
     filesystem::path filePath = currentDir / "../../test/pgn/games" / "fischer_spassky_game.pgn";
-
-    cout << "Filepath: " << filePath.string() << endl;
 
     // Concatenate all chess moves into a single string
     const string concatenatedChessMoves = Utils::parseFileAndConcatenateChessMoves(filePath.string());
