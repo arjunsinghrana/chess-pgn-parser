a586393cd8d2383d4c591c9ab2ed17e308b5a831
Arjun Singh <arjunsingh.rf@gmail.com>
2024-03-07 02:00:34 +0000

    refactor

--- src/utils.h
+++ src/utils.h
@@ -12,19 +12,21 @@ using namespace std;
 
 class Utils
 {
-public:
+private:
     static string removeSubstringInCurlyBrackets(const string& input);
     static string removeSubstringWithNumberAndThreeDots(const string& input);
     static string removeCharactersFromString(const string& characters, const string& input);
 
     static ChessMove parseSANMove(string& san);
-    static vector<ChessTurn> parseChessMoves(const string& line);
 
-    static string pieceToString(const Piece& piece);
     static Piece charToPiece(char piece);
 
     static string colorToString(const Color& color);
 
+public:
+    static vector<ChessTurn> parseChessMoves(const string& line);
+
+    static string pieceToString(const Piece& piece);
     static string chessPieceToString(const ChessPiece& chessPiece);
 };
 
